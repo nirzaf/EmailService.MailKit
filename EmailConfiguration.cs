b9d3872728151cf4e8fd97bd1ee9d5d346227c55
nirzaf <mfmfazrin1986@gmail.com>
2022-05-11 11:46:30 +0530

    commit chnages

---
+++ EmailConfiguration.cs
@@ -0,0 +1,11 @@
+﻿namespace EmailService.MailKit
+{
+    public class EmailConfiguration
+    {
+        public string SmtpServer { get; set; }
+        public int Port { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string From { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,72 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EmailService.MailKit;
+using MailKit.Net.Smtp;
+using Microsoft.Extensions.Configuration;
+using MimeKit;
+using static System.Console;
 
 
-//Create a Email Service using MailKit
+try
+{
+    var builder = new ConfigurationBuilder()
+        .AddJsonFile($"appsettings.json", true, true);
+
+    var config = builder.Build();
+
+    var emailHost = config["Host"];
+    var port = config["Port"];
+    var username = config["Username"];
+    var password = config["Password"];
+    var from = config["From"];
+
+    EmailConfiguration mailConfig = new()
+    {
+        SmtpServer = emailHost,
+        Port = Convert.ToInt32(port),
+        UserName = username,
+        Password = password,
+        From = from
+    };
+
+    MimeMessage mimeMessage = new();
+    mimeMessage.From.Add(new MailboxAddress(username, from));
+    mimeMessage.Importance = MessageImportance.High;
+    mimeMessage.Priority = MessagePriority.Urgent;
+
+    List <MailboxAddress> toList = new() {new MailboxAddress("Mohamed Fazrin", "mfmfazrin1986@gmail.com")};
+    mimeMessage.To.AddRange(toList);
+    mimeMessage.Subject = "Testing Mail Subject";
+
+    var msBuilder = new BodyBuilder
+    {
+        HtmlBody = $"<h2 style='color:red;'> Testing Mail Body </h2>"
+    };
+
+    mimeMessage.Body = msBuilder.ToMessageBody();
+
+
+    SendMailAsyncClass mail = new();
+    await mail.SendAsync(mimeMessage, mailConfig);
+    WriteLine("Mail sent successfully");
+
+    ReadLine();
+}
+catch (Exception exception)
+{
+    WriteLine(exception.Message);
+}
+
+
+
+public class SendMailAsyncClass
+{
+    public async Task SendAsync(MimeMessage message, EmailConfiguration smtp)
+    {
+        SmtpClient client = new();
+        await client.ConnectAsync(smtp.SmtpServer, smtp.Port, false);
+        // Note: since we don't have an OAuth2 token,
+        // disable the XOAUTH2 authentication mechanism to speed up the process.
+        client.AuthenticationMechanisms.Remove("XOAUTH2");
+        await client.AuthenticateAsync(smtp.From, smtp.Password);
+        await client.SendAsync(message);
+    }
+}
